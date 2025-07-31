97cfcc67d185ad6202800ea6c156863177c4ef25
arzaanK <arzaanK@gmail.com>
2025-07-31 22:24:23 +0530

    Update MediaGallery.tsx with hardcoded image URLs

--- get_cloudinary_folders.ps1
+++
@@ -1,3 +0,0 @@
-$auth = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes("878769551721862:0v3eRiejau1m-tdnK7xgoR1Cetc"))
-$response = Invoke-RestMethod -Uri https://api.cloudinary.com/v1_1/dhxetyrkb/folders -Headers @{Authorization="Basic $($auth)"}
-$response.folders | ForEach-Object { $_.name }
